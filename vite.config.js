7acd238cf72cbcc643101eadc32b72bf1c5937e5
TheAnnoying <drork2018@gmail.com>
2023-07-07 15:26:22 +0300

    Made assets a public vite folder and changed paths

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'index.html'),
+				main: resolve(__dirname, 'index.html')
 			},
 			output: {
 				entryFileNames: `assets/[name].js`,
-				chunkFileNames: `assets/[name].js`,
 				assetFileNames: `assets/[name].[ext]`
 			}
-		},
+		}
 	},
+	publicDir: 'assets',
+	plugins: [
+		topLevelAwait()
+	]
 });
