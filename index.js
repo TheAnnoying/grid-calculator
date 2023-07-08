f0c13d940c42c7e3fa2b11e233fc766e7f6c3855
TheAnnoying <drork2018@gmail.com>
2023-07-08 14:29:02 +0300

    Created disabled class for fonts that don't exist

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ fontChoiceButton.addEventListener("click", (e) => {
 
 				showModal("Choose font", `
 					<div class="text-white gap-4 grid grid-cols-2 place-items-center font-select cursor-pointer">${category.fonts.map(e => `
-						<div class="font${selectedCategory === category.id && selectedFont === e[0] ? " selected" : ""}" data-font="${e[0]}">
+						<div class="font${selectedCategory === category.id && selectedFont === e[0] ? " selected" : ""} ${!widths?.[category.id]?.[e[0]] ? "disabled" : ""}" data-font="${e[0]}">
 							<div style="font-family:${category.id}-${e[0]}; font-size: ${e[2] + "rem" ?? "4rem"};">ABC</div>
 						</div>
 					`).join("")}</div>
